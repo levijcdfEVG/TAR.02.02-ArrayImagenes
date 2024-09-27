cccb37287d1143ff38097c184a7d1e89067faf88
levijcdfEVG <levijosuecandeiasdefigueiredo.guadalupe@alumnado.fundacionloyola.net>
2024-09-27 08:33:25 +0200

    Se ha probado a añadir más imagenes

--- imagenes.php
+++ imagenes.php
@@ -3,4 +3,5 @@
     $arrayImagenes['alentejo'] = 'alentejo.jpeg';
     $arrayImagenes['montania'] = 'montania.jpeg';
     $arrayImagenes['playa'] = 'playa.jpeg';
+    $arrayImagenes['alentejo2'] = 'alentejo.jpeg';
 ?>
